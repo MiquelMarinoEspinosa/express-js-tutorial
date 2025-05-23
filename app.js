fcec28e4a9d61b38f69086c1419b32f3b0cc2940
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-23 14:45:58 +0000

    post create movies

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const fs = require('fs');
 let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
-//GET - api/movies
+app.use(express.json())
+
+//GET - api/v1/movies
 app.get('/api/v1/movies', (req, res) => {
     res.status(200).json({
         status: "success",
@@ -14,6 +16,26 @@ app.get('/api/v1/movies', (req, res) => {
             movies: movies
         }
     })
+})
+
+//POAR - api/v1/movies
+app.post('/api/v1/movies', (req, res) => {
+    //console.log(req.body);
+    const newId = movies[movies.length - 1].id + 1;
+
+    const newMovie = Object.assign({id: newId}, req.body)
+
+    movies.push(newMovie);
+
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                movie: newMovie
+            }
+        })
+    })
+    //res.send('Created');
 })
 
 //Create a server
