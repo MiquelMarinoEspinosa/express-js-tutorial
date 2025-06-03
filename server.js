51666a010c5622728f769ab2655bc7fd2cbaab04
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-03 14:12:17 +0000

    mongo connection

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({path: './config.env'})
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const app = require("./app");
 
 //console.log(app.get('env'));
 console.log(process.env);
@@ -10,6 +11,18 @@ console.log(process.env);
 //Create a server
 const port = process.env.PORT || 3000;
 
+mongoose
+  .connect(process.env.CONN_STR, {
+    useNewUrlParser: true,
+  })
+  .then((conn) => {
+    //console.log(conn);
+    console.log("DB Connections Successful");
+  })
+  .catch((error) => {
+    console.log("Some error has occurred");
+  });
+
 app.listen(port, () => {
-    console.log('server has started...');
+  console.log("server has started...");
 });
