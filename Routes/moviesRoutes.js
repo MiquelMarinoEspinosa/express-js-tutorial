e42b28ba121f34930ed58f8bfff880acb943ea61
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-16 14:11:42 +0000

    $unwind and $project stages and new operators

--- Routes/moviesRoutes.js
+++ Routes/moviesRoutes.js
@@ -7,6 +7,8 @@ router.route("/highest-rated").get(moviesController.highestRated);
 
 router.route("/movie-stats").get(moviesController.getMovieStats);
 
+router.route("/movies-by-genre/:genre").get(moviesController.getMovieByGenre);
+
 router
   .route("/")
   .get(moviesController.getAllMovies)
