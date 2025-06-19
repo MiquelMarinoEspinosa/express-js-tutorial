2cc560dba4bf2a5d53a8ad902b869cc4d3bd3930
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-19 13:42:52 +0000

    prod and dev error handling

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,8 +1,32 @@
-module.exports = (error, req, res, next) => {
-  const statusCode = error.statusCode || 500;
-  const status = error.status || "error";
+const devErrors = (res, error, statusCode) => {
   res.status(statusCode).json({
     status: statusCode,
     message: error.message,
+    stackTrace: error.stack,
+    error: error,
   });
+};
+
+const prodErrors = (res, error, statusCode) => {
+  if (error.isOperational) {
+    res.status(statusCode).json({
+      status: statusCode,
+      message: error.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong! Please try again later.",
+    });
+  }
+};
+
+module.exports = (error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+
+  if (process.env.NODE_ENV === "development") {
+    devErrors(res, error, statusCode);
+  } else if (process.env.NODE_ENV === "production") {
+    prodErrors(res, error, statusCode);
+  }
 };
