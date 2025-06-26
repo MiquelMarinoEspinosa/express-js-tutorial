799a7446a3dc0b903b753b72080107c7211fe849
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-26 14:02:52 +0000

    create jwt when user is created

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,11 +1,16 @@
 const User = require("../Models/userModel");
 const asyncErrorHandler = require("../Utils/asyncErrorHandler");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = asyncErrorHandler(async (req, res, next) => {
   const newUser = await User.create(req.body);
+  const token = jwt.sign({ id: newUser._id }, process.env.SECRET_STR, {
+    expiresIn: process.env.LOGIN_EXPIRES,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
